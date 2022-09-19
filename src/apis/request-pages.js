2cf8ab606614b6251fa0ab7d62a77db79a0abc71
wengtianchen <wengtianchen@foxmail.com>
2022-09-19 10:20:48 +0800

    fix(custom): 测试请求域名在生产环境的表现

--- src/apis/request-pages.js
+++ src/apis/request-pages.js
@@ -1,4 +1,4 @@
 import { get } from './request'
 export const getHealthCodeData = () => {
-  return get('/healthCode/codeCount')
+  return get('http://supervise.bmsoft.com:9080/supervise/healthCode/codeCount')
 }
