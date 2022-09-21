eed40db19b3ae54324a6e54e7a164b8ae408c59c
wengtianchen <wengtianchen@foxmail.com>
2022-09-21 14:57:54 +0800

    fix(custom): 防疫健康码
    修改防疫健康码请求api地址

--- src/apis/request-pages.js
+++ src/apis/request-pages.js
@@ -1,5 +1,5 @@
 import { get } from './request'
 export const getHealthCodeData = () => {
-  return get('http://supervise.bmsoft.com:9080/supervise/healthCode/codeCount')
+  return get('http://58.49.165.247:8081/18a9a2cc83ef4c74b0ecab7fb636b3ff')
   // return get('/supervise/healthCode/codeCount')
 }
